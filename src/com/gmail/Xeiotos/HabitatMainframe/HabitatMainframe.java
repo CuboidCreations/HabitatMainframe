2934d0e8563144a749e90c0fd407ba855b045eaa
Chris Soberon <Xeiotos@gmail.com>
2017-06-07 16:10:15 +0200

    Inital commit

---
+++ src/com/gmail/Xeiotos/HabitatMainframe/HabitatMainframe.java
@@ -0,0 +1,12 @@
+package com.gmail.Xeiotos.HabitatMainframe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Chris
+ */
+public class HabitatMainframe extends JavaPlugin {
+
+    
+}
